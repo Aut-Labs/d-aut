43342c47b0f2ac9e4c0f5ed7300d691727b5ef16
antant <ant.iv.ge@gmail.com>
2022-12-19 19:42:53 +0200

    feat: changed ipfs gateway

--- src/services/web3/env.ts
+++ src/services/web3/env.ts
@@ -1,6 +1,6 @@
 // eslint-disable-next-line import/no-mutable-exports
 export const env = {
-  REACT_APP_IPFS_URL: 'https://infura-ipfs.io/ipfs',
+  REACT_APP_IPFS_URL: 'https://ipfs.io/ipfs',
   REACT_APP_NFT_STORAGE_KEY:
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDIwQkEyNDNhNTU1YmY4YzI0MzViNzVmMTk0NmFDNWQ2QTY4QUQzMjgiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY0MzkwMjIzNDA2NywibmFtZSI6IlBhcnRuZXJzQXBwIn0.sG-6S0mNp0FQ_4SIimMChrMj4250ymEH58V09eXNY4o',
 };
